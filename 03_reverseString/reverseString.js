7721fa3685f67d6492db8de1e1006797a5d988e4
ind3ci5ive <39619831+ind3ci5ive@users.noreply.github.com>
2024-11-27 13:05:46 -0600

    Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,28 @@
-const reverseString = function() {
+// Create empty array to store string
+// Loop through the string
+// While looping, add each character *at the beginning* of the array so contents become reversed
+// Use ToString() to return the new array as a String value
 
+
+
+const reverseString = function(word) {
+
+    let originalWord = word;
+    let originalWordArray = [];
+
+    for(let i = 0; i < originalWord.length; i++) {
+
+        originalWordArray.unshift(originalWord.charAt(i));
+    }
+
+    let reversedWord = originalWordArray.join("")
+    return reversedWord
+    // console.log(reversedWord);
 };
+
+// reverseString('hello');
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
