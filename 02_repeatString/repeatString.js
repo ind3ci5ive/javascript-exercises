9f9d246a89ec4e009b52864db99716fa25af47ed
ind3ci5ive <39619831+ind3ci5ive@users.noreply.github.com>
2024-11-27 12:11:56 -0600

    Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,32 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let returnValue = '';
+
+    if (num > 0) {
+
+        for(let i = 0; i < num; i++) {
+
+            returnValue += string;
+        }
+
+    }
+
+    else if (num == 0) {
+
+        returnValue = '';
+    }
+
+    else {
+
+        returnValue = 'ERROR';
+    }
+
+    return returnValue;
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
