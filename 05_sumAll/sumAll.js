3c8ab5f108aa399f11cad1f1c57438c8ee2e7d5f
ind3ci5ive <39619831+ind3ci5ive@users.noreply.github.com>
2024-12-01 15:57:02 -0600

    Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,53 @@
-const sumAll = function() {
 
+// Create two values to hold each parameter
+// The first value will keep incrementing by 1 and add to itself each time
+// until the second value has finished it's loop
+// return the value 
+
+
+
+
+const sumAll = function (beginningNum, endingNum) {
+
+    let beginNum = beginningNum;
+    let endNum = endingNum;
+    let total = 0;
+
+
+    if (beginNum > endNum) {
+
+        let tempNum = beginNum;
+        beginNum = endNum;
+        endNum = tempNum;
+    }
+
+    else if (beginNum < 0 || endNum < 0) {
+
+
+        return "ERROR";
+    }
+
+    else if (beginNum - Math.floor(beginNum) !== 0 || endNum - Math.floor(endNum) !== 0) {
+
+        return "ERROR";
+    }
+
+    else if (typeof beginNum !== "number" || typeof endNum !== "number") {
+
+        return "ERROR";
+    }
+
+
+    for (i = beginNum; i <= endNum; i++) {
+
+        total += i;
+    }
+
+
+    return total;
 };
 
+
+console.log(sumAll(10, "90"));
 // Do not edit below this line
 module.exports = sumAll;
