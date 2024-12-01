c7e74310e14eb276ffdd3b95e7177669441f277b
ind3ci5ive <39619831+ind3ci5ive@users.noreply.github.com>
2024-12-01 16:43:24 -0600

    Complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
 
+
+
+
+const leapYears = function(year) {
+
+    let enteredYear = year;
+    let isLeapYear;
+
+    if (enteredYear % 4 == 0 && enteredYear % 100 != 0 || enteredYear % 400 == 0) {
+
+        
+        isLeapYear = true;
+        return isLeapYear;
+    }
+
+    else {
+
+        isLeapYear = false;
+        return isLeapYear;
+    }
 };
 
 // Do not edit below this line
